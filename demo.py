d7c15d76e26b84719bb8de2a1aa141e69561ec7e
Odysseas Sofikitis <o.sofikitis@gmail.com>
2024-02-26 14:03:12 +0200

    fine tunes demo

--- demo.py
+++ demo.py
@@ -24,7 +24,7 @@ c_table = np.array([[17, 18, 24, 47, 99, 99, 99, 99],
                    [99, 99, 99, 99, 99, 99, 99, 99],
                    [99, 99, 99, 99, 99, 99, 99, 99]])
 
-# load image
+## load image
 image = Image.open('baboon.png')
 image = np.array(image)
 
@@ -36,63 +36,93 @@ if dim_M != 0:
 if dim_N != 0:
   image = image[:, :-dim_N, :]
 
+## jpeg
 # convert to ycrcb
 imageY, imageCr, imageCb = fn.convert2ycrcb(image, [4, 2, 0])
 
 # convert to dct blocks
-blocks_y = {}
-blocks_cr = {}
-blocks_cb = {}
-for i in range(imageY.shape[0] // 8):
-  blocks_y[i] = {}
-  for j in range(imageY.shape[1] // 8):
+M_y = imageY.shape[0] // 8
+N_y = imageY.shape[1] // 8
+M_c = imageCr.shape[0] // 8
+N_c = imageCr.shape[1] // 8
+blocks_y = {i: {} for i in range(M_y)}
+blocks_cr = {i: {} for i in range(M_c)}
+blocks_cb = {i: {} for i in range(M_c)}
+
+for i in range(M_y):
+  for j in range(N_y):
     blocks_y[i][j] = fn.blockDCT(imageY[i * 8:(i + 1) * 8, j * 8: (j + 1) * 8])
 
-for i in range(imageCr.shape[0] // 8):
-  blocks_cr[i] = {}
-  blocks_cb[i] = {}
-  for j in range(imageCr.shape[1] // 8):
+for i in range(M_c):
+  for j in range(N_c):
     blocks_cr[i][j] = fn.blockDCT(imageCr[i * 8:(i + 1) * 8, j * 8: (j + 1) * 8])
     blocks_cb[i][j] = fn.blockDCT(imageCb[i * 8:(i + 1) * 8, j * 8: (j + 1) * 8])
 
 # quantize
-for i in range(imageY.shape[0] // 8):
-  for j in range(imageY.shape[1] // 8):
+for i in range(M_y):
+  for j in range(N_y):
     blocks_y[i][j] = fn.quantizeJPEG(blocks_y[i][j], y_table, 0.5)
 
-for i in range(imageCr.shape[0] // 8):
-  for j in range(imageCr.shape[1] // 8):
+for i in range(M_c):
+  for j in range(N_c):
     blocks_cr[i][j] = fn.quantizeJPEG(blocks_cr[i][j], c_table, 0.5)
     blocks_cb[i][j] = fn.quantizeJPEG(blocks_cb[i][j], c_table, 0.5)
 
-# encode
-symbols_y = {i: {} for i in range(imageY.shape[0] // 8)}
-symbols_cr = {i: {} for i in range(imageCr.shape[0] // 8)}
-symbols_cb = {i: {} for i in range(imageCr.shape[0] // 8)}
+# encode to run symbols
+symbols_y = {i: {} for i in range(M_y)}
+symbols_cr = {i: {} for i in range(M_c)}
+symbols_cb = {i: {} for i in range(M_c)}
+
 symbols_y[0][0] = fn.runLength(blocks_y[0][0], 0)
-for i in range(imageY.shape[0] // 8):
-  for j in range(imageY.shape[1] // 8):
+for i in range(M_y):
+  for j in range(N_y):
     if i == 0 and j == 0:
       continue
     prev = [i, j - 1] if j > 0 else [i - 1, imageY.shape[1] // 8 - 1]
-    symbols_y[i][j] = fn.runLength(blocks_y[i][j], blocks_y[prev[0]][prev[1]][0, 0])
+    dc_pred = blocks_y[prev[0]][prev[1]][0, 0]
+    symbols_y[i][j] = fn.runLength(blocks_y[i][j], dc_pred)
 
 symbols_cr[0][0] = fn.runLength(blocks_cr[0][0], 0)
 symbols_cb[0][0] = fn.runLength(blocks_cb[0][0], 0)
-for i in range(imageCr.shape[0] // 8):
-  for j in range(imageCr.shape[1] // 8):
+for i in range(M_c):
+  for j in range(N_c):
     if i == 0 and j == 0:
       continue
     prev = [i, j - 1] if j > 0 else [i - 1, imageCr.shape[1] // 8 - 1]
-    symbols_cr[i][j] = fn.runLength(blocks_cr[i][j], blocks_cr[prev[0]][prev[1]][0, 0])
-    symbols_cb[i][j] = fn.runLength(blocks_cb[i][j], blocks_cb[prev[0]][prev[1]][0, 0])
+
+    dc_pred = blocks_cr[prev[0]][prev[1]][0, 0]
+    symbols_cr[i][j] = fn.runLength(blocks_cr[i][j], dc_pred)
+
+    dc_pred = blocks_cb[prev[0]][prev[1]][0, 0]
+    symbols_cb[i][j] = fn.runLength(blocks_cb[i][j], dc_pred)
+
+# huffman encoding
+for i in range(M_y):
+  for j in range(N_y):
+    symbols_y[i][j] = fn.huffEnc(symbols_y[i][j], 'lum')
+
+for i in range(M_c):
+  for j in range(N_c):
+    symbols_cr[i][j] = fn.huffEnc(symbols_cr[i][j], 'chrom')
+    symbols_cb[i][j] = fn.huffEnc(symbols_cb[i][j], 'chrom')
 
 
 ## inverse
-# decode
+# decode huffman
+for i in range(M_y):
+  for j in range(N_y):
+    symbols_y[i][j] = fn.huffDec(symbols_y[i][j], 'lum')
+    exit(1)
+
+for i in range(M_c):
+  for j in range(N_c):
+    symbols_cr[i][j] = fn.huffDec(symbols_cr[i][j], 'chrom')
+    symbols_cb[i][j] = fn.huffDec(symbols_cb[i][j], 'chrom')
+
+# decode run symbols
 blocks_y[0][0] = fn.irunLength(symbols_y[0][0], 0)
-for i in range(imageY.shape[0] // 8):
-  for j in range(imageY.shape[1] // 8):
+for i in range(M_y):
+  for j in range(N_y):
     if i == 0 and j == 0:
       continue
     prev = [i, j - 1] if j > 0 else [i - 1, imageY.shape[1] // 8 - 1]
@@ -100,8 +130,8 @@ for i in range(imageY.shape[0] // 8):
 
 blocks_cr[0][0] = fn.irunLength(symbols_cr[0][0], 0)
 blocks_cb[0][0] = fn.irunLength(symbols_cb[0][0], 0)
-for i in range(imageCr.shape[0] // 8):
-  for j in range(imageCr.shape[1] // 8):
+for i in range(M_c):
+  for j in range(N_c):
     if i == 0 and j == 0:
       continue
     prev = [i, j - 1] if j > 0 else [i - 1, imageCr.shape[1] // 8 - 1]
@@ -109,12 +139,12 @@ for i in range(imageCr.shape[0] // 8):
     blocks_cb[i][j] = fn.irunLength(symbols_cb[i][j], blocks_cb[prev[0]][prev[1]][0, 0])
 
 # dequantize
-for i in range(imageY.shape[0] // 8):
-  for j in range(imageY.shape[1] // 8):
+for i in range(M_y):
+  for j in range(N_y):
     blocks_y[i][j] = fn.dequantizeJPEG(blocks_y[i][j], y_table, 0.5)
 
-for i in range(imageCr.shape[0] // 8):
-  for j in range(imageCr.shape[1] // 8):
+for i in range(M_c):
+  for j in range(N_c):
     blocks_cr[i][j] = fn.dequantizeJPEG(blocks_cr[i][j], c_table, 0.5)
     blocks_cb[i][j] = fn.dequantizeJPEG(blocks_cb[i][j], c_table, 0.5)
 
@@ -132,22 +162,22 @@ for i in range(imageCr.shape[0] // 8):
     image_Cb[i * 8:(i + 1) * 8, j * 8:(j + 1) * 8] = fn.iBlockDCT(blocks_cb[i][j])
 
 # convert to rgb
-imageRGB = fn.convert2rgb(image_Y, image_Cr, image_Cb, [4, 2, 0])
+image_rgb = fn.convert2rgb(image_Y, image_Cr, image_Cb, [4, 2, 0])
 
 
 ## display
-imageRGB = imageRGB / 255.0
+image_jpeg = image_rgb / 255.0
 image = image / 255.0
 
 plt.figure(figsize=(10, 5))
 
 plt.subplot(1, 2, 1)
-plt.imshow(imageRGB)
-plt.title('ImageRGB')
+plt.imshow(image_jpeg)
+plt.title('JPEG')
 
 plt.subplot(1, 2, 2)
 plt.imshow(image)
-plt.title('Image')
+plt.title('Original')
 
 plt.tight_layout()
 plt.show()
